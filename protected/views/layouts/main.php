6f2a05c151797eec7bbc2dd2993ca8833add9b3b
PradatiusD <DanielPrada2012@gmail.com>
2013-04-13 17:15:18 -0400

    Logo file and correct php file

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -17,7 +17,7 @@
 <div class="row" id="page">
 
 	<div id="header">
-		<div id="logo" class="small-3 small-centered columns"><img src="assets/logo.png" alt=""><span style="display:none"><?php echo CHtml::encode(Yii::app()->name); ?></span></div>
+		<div id="logo" class="small-3 small-centered columns"><img src="img/logo.png" alt=""><span style="display:none"><?php echo CHtml::encode(Yii::app()->name); ?></span></div>
 	</div><!-- header -->
 
 	<div id="mainmenu" class="large-12 columns">
